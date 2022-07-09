50f3fd1d8f9adec46f76d26e4341eea4bbef0646
Erick Santos <ericksantodev@gmail.com>
2022-07-09 18:15:42 -0300

    Possibilidade de adição, mudança de 'a fazer' para 'feito' e exclusão.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+
 const defaultPadding = 16.0;
+
+void showCustomSnackBar({
+  required BuildContext context,
+  Duration? duration,
+  required String title,
+  required String snackBarActionLabel,
+  required VoidCallback onSnackBarActionClicked,
+}) {
+  var snackBar = SnackBar(
+    content: Text(title),
+    duration: duration ?? Duration(seconds: 4),
+    action: SnackBarAction(
+      label: 'Desfazer',
+      onPressed: onSnackBarActionClicked,
+    ),
+  );
+
+  ScaffoldMessenger.of(context).removeCurrentSnackBar();
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:todolistwithprogressbar/constants.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,31 +13,85 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List todoList = [];
+  late List todoList;
   final todoController = TextEditingController();
+  late int indexLastRemoved;
+  late Map<String, dynamic> lastRemoved;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Todo List')),
-      body: Builder(
-        builder: (context) => Column(
-          children: [
-            buildForm(),
-            Expanded(
-              child: buildList(),
-            ),
-          ],
-        ),
-      ),
-    );
+  void initState() {
+    super.initState();
+
+    readData().then((value) {
+      setState(() => todoList = jsonDecode(value!));
+    });
   }
+
+  Future<String?> readData() async {
+    try {
+      final file = await openFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  void addTask() {
+    setState(() {
+      Map<String, dynamic> newTask = {};
+
+      newTask['title'] = todoController.text;
+      newTask['done'] = false;
+
+      todoController.clear();
+
+      todoList.add(newTask);
+
+      saveData();
+    });
+  }
+
+  Future<File> saveData() async {
+    String data = json.encode(todoList);
+    final file = await openFile();
+
+    return file.writeAsString(data);
+  }
+
+  Future<File> openFile() async {
+    final appPath = await getApplicationDocumentsDirectory();
+
+    return File("${appPath.path}/todoList.json");
+  }
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text('Todo List')),
+          body: Builder(
+            builder: (context) => Column(
+              children: [
+                buildForm(),
+                Expanded(
+                  child: buildList(),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
 
   Widget buildForm() => Container(
         padding: EdgeInsets.fromLTRB(defaultPadding, 8.0, defaultPadding, 24.0),
         child: Row(
           children: [
-            Expanded(child: TextField(controller: todoController)),
+            Expanded(
+              child: TextField(
+                controller: todoController,
+                maxLength: 40,
+                decoration: InputDecoration(labelText: 'Nova tarefa'),
+              ),
+            ),
             SizedBox(width: defaultPadding),
             SizedBox.square(
               dimension: 45.0,
@@ -48,22 +106,87 @@ class _HomePageState extends State<HomePage> {
 
   void addClicked() {
     if (todoController.text.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Vazio')),
+      showCustomSnackBar(
+        context: context,
+        title: 'Insira uma tarefa para adicionar',
+        snackBarActionLabel: 'Ok',
+        onSnackBarActionClicked: () =>
+            ScaffoldMessenger.of(context).removeCurrentSnackBar(),
       );
+    } else {
+      addTask();
     }
   }
 
-  Widget buildList() => ListView.builder(
-        padding: EdgeInsets.fromLTRB(
-          defaultPadding,
-          4.0,
-          defaultPadding,
-          defaultPadding,
+  Widget buildList() => RefreshIndicator(
+        onRefresh: onRefresh,
+        child: ListView.builder(
+          padding: EdgeInsets.only(
+            top: 4.0,
+            bottom: defaultPadding,
+          ),
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            final item = todoList[index];
+
+            return buildTask(context, index, item);
+          },
         ),
-        itemCount: 25,
-        itemBuilder: (context, index) {
-          return Card(child: ListTile(title: Text('Index $index')));
+      );
+
+  Future<void> onRefresh() async {}
+
+  Widget buildTask(BuildContext context, int index, dynamic item) =>
+      Dismissible(
+        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+        background: Container(
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment(0.85, 0.0),
+            child: Icon(Icons.delete_sweep, color: Colors.white),
+          ),
+        ),
+        direction: DismissDirection.endToStart,
+        child: buildWidget(item),
+        onDismissed: (dismissed) {
+          setState(() {
+            lastRemoved = Map.from(item);
+            indexLastRemoved = index;
+
+            todoList.removeAt(index);
+
+            saveData();
+
+            showCustomSnackBar(
+              context: context,
+              title: 'A tarefa "${lastRemoved['title']}" foi excluída',
+              snackBarActionLabel: 'Desfazer',
+              onSnackBarActionClicked: () {
+                setState(() {
+                  todoList.insert(indexLastRemoved, lastRemoved);
+                  saveData();
+                });
+              },
+            );
+          });
+        },
+      );
+
+  Widget buildWidget(dynamic item) => CheckboxListTile(
+        value: item['done'],
+        secondary: CircleAvatar(
+          backgroundColor: Theme.of(context).primaryColor,
+          child: Icon(
+            item['done'] ? Icons.check : Icons.error,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(item['title']),
+        onChanged: (value) {
+          setState(() {
+            item['done'] = value;
+            saveData();
+          });
         },
       );
 }
